d71eca8d0c3c47c1622810e8d828cb1a65d85769
philipredstone <rothstein@prtex.de>
2021-02-04 17:36:44 +0100

    help command rewrite

--- commands/general/help.js
+++ commands/general/help.js
@@ -4,61 +4,46 @@ class HelpCommand extends Command {
     constructor() {
         super('help', {
             aliases: ['help'],
-            description: "Returns a list of all commands"
+            args: [
+                {
+                    id: 'command'
+                }
+            ],
+            description: "Returns a list of all commands \n   Usage: `###help (<command>)`"
         });
     }
 
-    async exec(message) {
+    async exec(message, args) {
         message.delete();
-        const embed = this.client.util
+        const commandEmbed = this.client.util
             .embed()
             .setColor(0xDDD57A)
             .setTitle('Command List')
-            .setFooter("Requested by " + message.member.user.tag, message.member.user.avatarURL())
+            .setFooter(`Requestet by ${message.member.user.tag}`, message.member.user.avatarURL())
         const modules = Array.from(this.client.commandHandler.modules, ([name, value]) => ({ name, value }));
-        const commandz = message.content.slice(this.client.commandHandler.prefix(message).length + 4).trim().replace("+","");
-        const error = this.client.util
-            .embed()
-            .setColor(0xd92929)
-            .setTitle(message.guild.prefix+commandz+":")
-            .setFooter("Requested by " + message.member.user.tag, message.member.user.avatarURL())
-
-        let found = false;
-        let commandlist = "Prefix: `"+message.guild.prefix+"`\n"; 
-        modules.forEach(element => {
-            let commands = [];
-            if(element.value.ownerOnly)
+        let commandlist = "";
+        modules.forEach(command => {
+            if(command.value.ownerOnly)
                 return;
-            element.value.aliases.forEach(command => {
-                if(commandz != "") {
-                    if(command == commandz) {
-                        commands.push(command)
-                        embed.setTitle(`${message.guild.prefix}${command}:`);
-                        embed.setDescription((element.value.description).replace("###",this.client.commandHandler.prefix(message)),false)
-                        found = true;
+            let aliases = command.value.aliases;
+            if(args.command) {
+                aliases.forEach(alias => {
+                    if(alias == args.command) {
+                        commandEmbed.setTitle(this.client.commandHandler.prefix(message)+args.command+":");
+                        commandEmbed.setDescription(command.value.description.replace("###",this.client.commandHandler.prefix(message)))
                     }
-                } else {
-                    commands.push(command)
-                }
-            });
-            if(commandz == "") {
-                let commands_string;
-                if(commands.length > 1)
-                    commands_string = commands.join('|')
-                else
-                    commands_string = commands[0]
-                commandlist += `\n \`${commands_string}\``;
+                });
+            } else {
+                let aliaslist = [];
+                aliases.forEach(alias => {
+                    aliaslist.push(alias);
+                });
+                commandlist += "\n`"+aliaslist.join("|")+"`";
             }
         });
-        if(commandlist != "") {
-            found = true;
-            embed.setDescription(commandlist + "\n\n `help <command>` to get more help!")
-        }
-        
-        if(found)
-            return message.channel.send(embed);
-        else
-            return message.channel.send(error.setDescription("Error: Command not found!"));
+        if(commandlist != "")
+            commandEmbed.setDescription(`Prefix: \`${message.guild.prefix}\`\n${commandlist}\n\n \`help <command>\` to get command specific help!\``)
+        message.channel.send(commandEmbed)
     }
 }
 
